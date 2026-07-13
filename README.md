a252688dc1c31ff02ebf0a10204250b436d6aff5
Tadiwa Pfachi <pfachitadiwanashedavid@gmail.com>
2026-07-13 16:54:17 +0200

    update: added custom dir path

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # dir_manager
 
 list all never opened files in a directory
+
+usage
+`text
+python main.py --dir directory-to-scan
+`
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 from datetime import datetime, timedelta 
+import argparse
 
-current_dir = os.getcwd()
-dir_files = os.listdir()
+parser = argparse.ArgumentParser(description="Directory file manage")
+
+parser.add_argument("--dir", type=str, help="the directory to scan")
+
+args = parser.parse_args()
 
 def last_access_time(info):
     """get the last access time"""
@@ -19,28 +23,34 @@ def last_mod_time(info):
 
 
 def get_never_used_files(path):
-    """Return a list of never opened files 
-    """
+    """Return a list of never opened files"""
     list = []
-    for entry in os.scandir(path):
-        try:
-            is_dir = entry.is_dir(follow_symlinks=False)
-        except OSError as error:
-            print('Error calling is_dir():', error, file=sys.stderr)
-            continue
-        if is_dir:
-            
-            list.append(entry.path)
-            get_never_used_files(entry.path)
-        else:
+    try:
+        for entry in os.scandir(path):
             try:
-                info = entry.stat(follow_symlinks=False)
-                
-                if last_access_time(info) == last_mod_time(info):
-
-                    list.append(entry.path)
+                is_dir = entry.is_dir(follow_symlinks=False)
             except OSError as error:
-                print('Error calling stat():', error, file=sys.stderr)
+                print('Error calling is_dir():', error, file=sys.stderr)
+                continue
+            if is_dir:
+            
+                list.append(entry.path)
+                get_never_used_files(entry.path)
+            else:
+                try:
+                    info = entry.stat(follow_symlinks=False)
+                
+                    if last_access_time(info) == last_mod_time(info):
+
+                        list.append(entry.path)
+                except OSError as error:
+                    print('Error calling stat():', error, file=sys.stderr)
+    except NotADirectoryError as error:
+        print('The path is not a folder: ' + path)
     return list
 
-print(*get_never_used_files("."), sep="\n")
+path = args.dir or "."
+
+
+ 
+print(*get_never_used_files(path), sep="\n")
